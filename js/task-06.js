f64d4f238724cf2dfd43b33677e4e1567095a7b0
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-01 20:51:27 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,9 @@
-const validation = document.querySelector('#validation-input');
+const validationRef = document.querySelector('#validation-input');
 
-validation.addEventListener('blur', onValidationCheck);
-
-function onValidationCheck(event) {
-    validation.classList.remove(`invalid`, `valid`);
-    if (event.currentTarget.value.length === Number(validation.dataset.length)) {
-        validation.classList.replace(`valid`);
-    } else {
-        validation.classList.add(`invalid`);
+validationRef.addEventListener('blur', onValidationCheck);
+function onValidationCheck (event) {
+    validationRef.classList.add(`invalid`);
+    if (event.currentTarget.value.length === Number(validationRef.dataset.length)) {
+        validationRef.classList.replace(`invalid`, `valid`);
     }
 }
