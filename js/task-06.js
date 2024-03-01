763f3dd9c0bef146b658889fe4d522de5d3feb80
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-01 21:01:29 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,12 @@
 const validationRef = document.querySelector('#validation-input');
 
 validationRef.addEventListener('blur', onValidationCheck);
-function onValidationCheck (event) {
-    validationRef.classList.add(`invalid`);
+
+function onValidationCheck(event) {
+    validationRef.classList.add('invalid');
     if (event.currentTarget.value.length === Number(validationRef.dataset.length)) {
-        validationRef.classList.replace(`invalid`, `valid`);
+        validationRef.classList.replace('invalid', 'valid');
+    } else {
+        validationRef.classList.remove('valid'); // Додано видалення класу 'valid', якщо відбулася зміна
     }
 }
