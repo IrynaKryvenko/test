84ffbdc6311560dd350ee1705c4e95541c10c4ca
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-01 19:36:13 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const bodyPage = document.querySelector('body');
+
+changeBtn.addEventListener('click', onChangeBtn);
+
+function onChangeBtn() {
+  const mainColor = getRandomHexColor();
+  bodyPage.style.backgroundColor = mainColor;
+  colorSpan.textContent = mainColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
