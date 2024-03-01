f3c9667077c256879a9fc8486b08592f25c45900
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-01 19:53:45 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,8 +5,6 @@ validation.addEventListener('blur', onValidationCheck);
 function onValidationCheck(event) {
     validation.classList.add(`invalid`);
     if (event.currentTarget.value.length === Number(validation.dataset.length)) {
-        validation.classList.add(`valid`);
-    } else {
-        validation.classList.add(`invalid`);
-    }
+        validation.classList.replace(`invalid`, `valid`);
+    } 
 }
