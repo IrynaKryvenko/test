7a78c4670606c30b37e65a0cc40e584b56475de1
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-05 21:23:03 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -29,7 +29,7 @@ function onMouseClick(event) {
     }
 
     const lightBox = new SimpleLightbox('.gallery a', {
-        captionData: 'alt',
+        captionsData: 'alt',
         captionDelay: 250,
         captionType: 'alt',
         captionPosition: 'bottom',
