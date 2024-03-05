e3419af68f0eee17f4a76a7c1256121805eeaf7c
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-03-05 21:00:15 +0200

    +

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+}
 
 console.log(galleryItems);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captionData: 'alt',
+    captionDelay: 250,
+    captionType: 'alt',
+    captionPosition: 'bottom',
+});
